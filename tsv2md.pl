a8b80b0737cdb566188aaa2186e119bacc0a8a48
14mission <nkibre@gmail.com>
2026-05-01 14:31:14 -0700

    add docs

--- tsv2md.pl
+++ tsv2md.pl
@@ -1,4 +1,12 @@
 #!/usr/bin/env perl
+use strict;
+# one way to use this:
+# pipe a tsv through it
+# copy the output
+# open in vscode
+# hit ctrl-k then m, tell vscode it's markdown
+# then ctrl-shift-v to see a formatted preview
+# then you can paste that wherever
 while(my $ln = <STDIN>) {
   @cols = split /\t/, $ln;
   $cols[-1] =~ s/\s+$//gs;
