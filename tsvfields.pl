e8fe9c62beb8c4c7f9ccf2256a367bd397f552ef
Nick Kibre <nkibre@gmail.com>
2026-09-05 07:17:21 -0700

    add -p(atternmode) and -(explicit)n(ullmode)

--- tsvfields.pl
+++ tsvfields.pl
@@ -12,9 +12,15 @@ binmode STDOUT, ":utf8";
 my @colnames;
 my @filenames;
 my $robust;
+my $patmode;
+my $nullmode;
 foreach my $arg (@ARGV) { 
     if($arg =~ m/^-r$/) {
         $robust = 1;
+    } elsif($arg =~ m/^-p$/) {
+        $patmode = 1;
+    } elsif($arg =~ m/^-n$/) {
+        $nullmode = 1;
     } elsif($arg =~ m/\.(csv|tsv|txt)$/) {
         push @filenames, $arg;
     } else {
@@ -23,11 +29,15 @@ foreach my $arg (@ARGV) {
 }
 if (@filenames == 0) { push @filenames, "-"; }
 
+if ($patmode) { @colnames = map '(?i).*'.$_.'.*', @colnames; }
+
 foreach my $fn (@filenames) { 
     print "READ: $fn\n";
     my $in = new readtsv($fn,map { $_, $_ } @colnames);
     $in->{robust} = $robust;
     while( my $row = $in->getln() ) {
-        print join("\t",map $row->{$_},@colnames),"\n";
+        print join("\t",map {
+          $nullmode && $row->{$_} eq "" ? "NULL" : $row->{$_}
+        } @colnames),"\n";
     }
 }
